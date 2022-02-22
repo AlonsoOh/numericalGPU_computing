1302b00524d8df0544e8b52e8e0923e424dab794
AlonsoOh <jrboyosa1126@naver.com>
2022-02-22 21:16:22 +0900

    초기 커밋.

---
+++ Test_Platform_20/main_Test_Platform_20.cpp
@@ -0,0 +1,23 @@
+#pragma warning(disable : 4996)
+
+//
+//  main_Test_Platform_20.cpp
+//  Test_Platform_20
+//
+//  Written for CSEG437_CSE5437
+//  Department of Computer Science and Engineering
+//  Copyright ©2020 Sogang University. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <CL/cl.h>
+
+#include "Util/my_OpenCL_util_2.h"
+
+int main(void) {
+    show_OpenCL_platform();
+    
+    return 0;
+}
+
